a72eaa7ff82e125af0dabb5c2f149ef1af9a3165
Vy Nguyen <nguyenluminhvy@gmail.com>
2026-03-13 15:15:14 +0700

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Spa Booking')
+    .setDescription('The spa booking API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
