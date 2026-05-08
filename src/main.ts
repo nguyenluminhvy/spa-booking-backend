1e155068caa95c26903365d2138c90af4f50d2f2
Vy Nguyen <nguyenluminhvy@gmail.com>
2026-05-08 21:57:28 +0700

    app config Cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Spa Booking')
